7100cdf70dc083410086937af65b3949a6433762
heartandkarma <heartandkarma@gmail.com>
2022-03-30 12:24:51 -0700

    got ShowForm displaying the proper items required

--- src/components/ShowForm.js
+++ src/components/ShowForm.js
@@ -4,6 +4,34 @@ const ShowForm = () => {
   return (
     <div>
       <h1>ShowForm</h1>
+      <form>
+        <label>
+          Name:
+          <input type='text' name='name' placeholder='name' value='' />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type='email' name='email' placeholder='email' value='' />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type='password'
+            name='password'
+            placeholder='password'
+            value=''
+          />
+        </label>
+        <br />
+        <label>
+          <input type='checkbox' name='tos' value='' />I agree to the terms of
+          service
+        </label>
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 };
